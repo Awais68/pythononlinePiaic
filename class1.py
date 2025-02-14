ac40ef64183d64585659729172a380b8fd781a84
Awais68 <hamza768@gmail.com>
2025-02-14 22:09:22 +0500

    First Class python done

---
+++ class1.py
@@ -0,0 +1,34 @@
+# # Variables
+
+# name : str  = 700
+# print(name) # print
+# print(type(name)) # type
+# print(id(name)) #physical address
+# # print([i for i in dir(name) if "___" not in i ])
+
+
+# # list
+
+# nameList : list[str]  = ['awais', 'nadeem', 'company', 770 ]
+# print(nameList) # print
+# print(type(nameList)) # type
+# print(id(nameList)) #physical address
+# # print([i for i in dir(name) if "___" not in i ])
+
+
+# # Tuple
+# name : tuple[str, int, float]  = ['Pakistan', 8, 0.5 , "januu"]
+# print(name) # print
+# print(type(name)) # type
+# print(id(name)) #physical address
+
+# Any
+# nameAny : any  = ['Pakistan', 8, 0.5 , "januu"]
+# print(nameAny) # print
+# print(type(nameAny)) # type
+# print(id(nameAny)) #physical address
+
+nameAny : any  = "pakista"
+print(nameAny) # print
+print(type(nameAny)) # type
+print(id(nameAny)) #physical address
