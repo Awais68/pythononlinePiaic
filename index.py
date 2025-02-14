fb771faf57e541436afd6c2d6d03842acc278446
Awais68 <hamza768@gmail.com>
2025-02-14 21:02:46 +0500

    first commit

---
+++ index.py
@@ -0,0 +1,13 @@
+
+a = 25,
+b = 96,
+
+
+print(a + b)
+print("a + b")
+print("Hello World")
+print("Sir ")
+print("Zia")
+print("Thanks Alot ")
+print("We proud to You ")
+print("Allah Bless you more")
